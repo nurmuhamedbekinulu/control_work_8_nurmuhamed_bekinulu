bda5510db40fce4c701932a1e02edf916bf1c8ad
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-03-25 17:13:32 +0600

    CRUD операции для Отзывов

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from webapp.views.products import ProductDetail, ProductUpdateView, \
     ProductCreateView, ProductDeleteView
-from webapp.views.reviews import ReviewCreateView
+from webapp.views.reviews import ReviewCreateView, ReviewDetail, \
+    ReviewDeleteView, ReviewUpdateView
 from webapp.views.base import IndexView, IndexRedirectView
 
 urlpatterns =[
@@ -13,5 +14,9 @@ urlpatterns =[
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/<int:pk>/confirm_delete/', ProductDeleteView.as_view(), name='confirm_delete'),
-    path('review/add/<int:pk>/', ReviewCreateView.as_view(), name='add_review')
+    path('review/add/<int:pk>/', ReviewCreateView.as_view(), name='add_review'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review_detail'),
+    path('review/<int:pk>/update/', ReviewUpdateView.as_view(), name='review_update'),
+    path('review/<int:pk>/delete/', ReviewDeleteView.as_view(), name='review_delete'),
+    path('review/<int:pk>/confirm_delete/', ReviewDeleteView.as_view(), name='review_delete'),
 ]
--- webapp/views/reviews.py
+++ webapp/views/reviews.py
@@ -1,13 +1,15 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import DeleteView, CreateView, UpdateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 from webapp.models import Product, Review
 from webapp.forms import ReviewForm
 
 
 
-class ReviewCreateView(CreateView):
+class ReviewCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     template_name = 'review_create.html'
     model = Review
     form_class = ReviewForm
@@ -19,3 +21,23 @@ class ReviewCreateView(CreateView):
         review.save()
         return redirect('product_detail', pk=product.pk)
 
+
+
+class ReviewDetail(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
+    template_name = 'review.html'
+    model = Review
+
+
+class ReviewUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    template_name = 'review_update.html'
+    form_class = ReviewForm
+    model = Review
+
+    def get_success_url(self):
+        return reverse('review_detail', kwargs={'pk': self.object.pk})
+
+
+class ReviewDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
+    template_name = 'review_confirm_delete.html'
+    model = Review
+    success_url = reverse_lazy('index')
